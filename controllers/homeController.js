0b8361775360fcd23682fad3e456d692591e9d61
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-06-25 22:10:00 +0700

    Cleanup 1

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,16 +4,6 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.homeRoute = async (req, res, next) => {
-    // BELAJAR REDIS
-    // const data = {
-    //     name: "Bagus Dwi Utama"
-    // }
-    // client.set('paijo:paijo', JSON.stringify(data))
-
-    // const hale = await client.get('login_portal:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImR3aXV0YW1hYmFndXNAZ21haWwuY29tIiwidXNlcklkIjoxLCJpYXQiOjE1NjE0MzAyMzMsImV4cCI6MTU2MTQ5MDIzM30.0bStIgquBGitctReu2KBP5H7pGKYRo3pgM9wW3ilXxE', function (err, response) {
-
-    // });
-
     res.status(200).json({
         posts: [
             {
@@ -23,7 +13,6 @@ exports.homeRoute = async (req, res, next) => {
             }
         ]
     });
-
 }
 
 
