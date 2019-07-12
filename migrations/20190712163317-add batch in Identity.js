4449a7c6b297be5b44bb9d12ea809fc87d5408c5
baguskah <dwiutamabagus@gmail.com>
2019-07-13 00:08:29 +0700

    add seed for peserta

---
+++ migrations/20190712163317-add batch in Identity.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return Promise.all([
+      queryInterface.addColumn('Identities', 'batchFim', { type: Sequelize.STRING }),
+      queryInterface.addColumn('Identities', 'regional', { type: Sequelize.STRING })
+
+   ]);
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return Promise.all([
+      queryInterface.removeColumn('Identities', 'batchFim'),
+      queryInterface.removeColumn('Identities', 'regional'),
+
+    ]);
+  }
+};
