2a5ebbc0891f2bef1571df8a4db8d5341cfdf463
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2019-07-17 09:01:21 +0700

    STP (#106)
    * Change wording
    
    * Update wording
    
    * Adding file and icon seeder

--- seeders/20190708162106-tunnel.js
+++ seeders/20190708162106-tunnel.js
@@ -9,7 +9,8 @@ module.exports = {
         description:'peserta FIM yang akan menjadi kader inti dalam berbagai aktivitas FIM, baik ditingkat pusat maupun regional. Jalur ini dikhususkan bagi alumni FIM 20 yang telah mengikuti pelatihan wilayah pada 2018 lalu untuk meningkatkan kapasitas dan memperluas jejaring dalam skala nasional. Peserta dari jalur ini mendapat peluang kuota sebesar 70%',
         createdBy: 1,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        urlPicture: 'https://res.cloudinary.com/fim-indonesia/image/upload/v1563328023/icon/NG_-_IKON_JALUR_MASUK_-_WEB_-_ILUSTRASI.png'
       },
       {
         id:2,
@@ -17,7 +18,8 @@ module.exports = {
         description:'calon peserta FIM yang merupakan pimpinan/aktivis atau calon pemimpin kampus yang memiliki misi untuk mendapat posisi strategis di organisasi kemahasiswaan. Peserta dari jalur ini diharapkan memperkuat jejaring stategis & kolaborasi pemimpin kampus untuk memberikan impact yang lebih nyata. FIM terbuka bagi setiap calon peserta dengan berbagai model organisasi asalkan sepakat untuk open mind dan menjaga koridor yang terstruktur dalam menghadapi perbedaan.',
         createdBy: 1,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        urlPicture: 'https://res.cloudinary.com/fim-indonesia/image/upload/v1563328023/icon/CL_-_IKON_JALUR_MASUK_-_WEB_-_ILUSTRASI.png'
       },
       {
         id:3,
@@ -25,7 +27,8 @@ module.exports = {
         description: 'calon peserta FIM yang merupakan pegiat yang fokus membangun kapasitas atau pemberdayaan masyarakat lokal di lingkungan daerah tempat tinggalnya. Jalur ini diharapkan bisa memberikan exposure yang lebih baik bagi para pejuang kerja-kerja basis, sekaligus memperbesar skala dan impact pekerjaannya. Aktivis yang dimaksud tidak terbatas pada kegiatan sosial saja, melainkan juga kegiatan ekonomi, politik, budaya, dan lainnya yang memiliki akar kuat di masyarakat',
         createdBy: 1,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        urlPicture: 'https://res.cloudinary.com/fim-indonesia/image/upload/v1563328023/icon/LL_-_IKON_JALUR_MASUK_-_WEB_-_ILUSTRASI.png'
       },
       {
         id:4,
@@ -33,7 +36,8 @@ module.exports = {
         description: 'calon peserta yang berasal dari profesional muda yang telah bekerja selama 1-5 tahun setelah lulus kuliah. Peserta dari jalur ini diharapkan bisa mengamplifikasi FIM kepada jejaring profesional secara lebih luas, agar mampu meningkatkan social impact dari berbagai program FIM. Bagi profesional muda yang berasal dari perusahaan yang masuk dalam 500 Fortune Indonesia akan menjadi prioritas.',
         createdBy: 1,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        urlPicture: 'https://res.cloudinary.com/fim-indonesia/image/upload/v1563328024/icon/YP_-_IKON_JALUR_MASUK_-_WEB_-_ILUSTRASI.png'
       },
       {
         id:5,
@@ -41,7 +45,8 @@ module.exports = {
         description: 'merupakan calon peserta dari jalur ini menyasar pada anak muda yang bercita-cita untuk menjadi expert/specialist di bidang/keilmuan tertentu. Jalur ini diharapkan bisa mengakselerasi proses transfer knowledge dan network creation dalam mencetak expert Indonesia di masa depan Bagi calon peserta yang juga pebisnis, bisa mendaftar melalui jalur ini untuk saling memperkuat benefit yang didapatkan dari network FIM',
         createdBy: 1,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        urlPicture: 'https://res.cloudinary.com/fim-indonesia/image/upload/v1563328024/icon/YE_-_IKON_JALUR_MASUK_-_WEB_-_ILUSTRASI.png'
       },
       {
         id:6,
@@ -49,7 +54,8 @@ module.exports = {
         description: 'bagi calon peserta merupakan anak muda yang mengambil karir sebagai aparatur sipil negara. Jalur ini diharapkan mampu mengkonsolidasikan berbagai ide dan inisiatif jejaring FIM mengenai reformasi birokrasi ataupun perbaikan layanan bagi masyarakat. FIM percaya bahwa mendorong perubahan birokrasi / pemerintahan oleh anak-anak muda merupakan kontribusi yang menjadi prioritas aksi nyata saat ini.',
         createdBy: 1,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        urlPicture: 'https://res.cloudinary.com/fim-indonesia/image/upload/v1563328023/icon/PS_-_IKON_JALUR_MASUK_-_WEB_-_ILUSTRASI.png'
       },
       {
         id:7,
@@ -57,7 +63,8 @@ module.exports = {
         description: 'jalur khusus yang diadakan untuk para calon pemimpin Indonesia di masa depan yang berasal dari militer (AD, AL, AU, dan polisi). Calon peserta merupakan top performer dari berbagai jalur di akademi militer dan kepolisian',
         createdBy: 1,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        urlPicture: 'https://res.cloudinary.com/fim-indonesia/image/upload/v1563328023/icon/MI_-_IKON_JALUR_MASUK_-_WEB_-_ILUSTRASI.png'
       },
       {
         id:8,
@@ -65,7 +72,8 @@ module.exports = {
         description: 'diperuntukan bagi calon peserta FIM yang mewakili spirit dan karakter generasi millennial sebagai digital influencer, tech savvy dengan jangkauan network yang luas. Peserta dari jalur ini memiliki misi untuk memperkuat digital movement dari berbagai kampanye program FIM. FIM percaya bahwa masa depan Indonesia dipengaruhi oleh banyaknya berbagai kampanye kebaikan berbasis online. Digital footprint FIM diharapkan bisa menjadi role model bagi gerakan anak muda lainnya.',
         createdBy: 1,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        urlPicture: 'https://res.cloudinary.com/fim-indonesia/image/upload/v1563328023/icon/IF_-_IKON_JALUR_MASUK_-_WEB_-_ILUSTRASI.png'
       }
     ], {});
   },
