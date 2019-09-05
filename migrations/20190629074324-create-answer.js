ad12dcd0ad4f20a7b579174b40deff477c34c451
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-09-06 06:23:28 +0700

    Change file migration:

--- migrations/20190629074324-create-answer.js
+++ migrations/20190629074324-create-answer.js
@@ -14,7 +14,7 @@ module.exports = {
       ktpNumber: {
         type: Sequelize.STRING
       },
-      TunnelId: {
+      tunnelId: {
         type: Sequelize.INTEGER
       },
       createdBy: {
--- migrations/20190629080241-create-summary.js
+++ migrations/20190629080241-create-summary.js
@@ -11,7 +11,7 @@ module.exports = {
       ktpNumber: {
         type: Sequelize.STRING
       },
-      TunnelId: {
+      tunnelId: {
         type: Sequelize.INTEGER
       },
       batchFim: {
