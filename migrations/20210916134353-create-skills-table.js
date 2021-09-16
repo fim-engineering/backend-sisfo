8ad700aef232b0b877edde6c0fb1fb17870f0373
Luthfi Rahman <luthfirahman.id@gmail.com>
2021-09-16 21:10:26 +0700

    Init migration

---
+++ migrations/20210916134353-create-skills-table.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('Skills', { 
+       id: {
+         allowNull: false,
+         autoIncrement: true,
+         primaryKey: true,
+         type: Sequelize.INTEGER
+       },
+       userId: {
+         type: Sequelize.INTEGER
+       },
+       isAbleVideoEditing: {
+         type: Sequelize.BOOLEAN,
+         defaultValue: 0
+       },
+       videoEditingPortofolioUrl: {
+         type: Sequelize.STRING
+       },
+       firstCertificateUrl: {
+         type: Sequelize.STRING
+       },
+       secondCertificateUrl: {
+         type: Sequelize.STRING
+       },
+       thirdCertificateUrl: {
+         type: Sequelize.STRING
+       },
+       createdAt: {
+         allowNull: false,
+         type: Sequelize.DATE
+       },
+       updatedAt: {
+         allowNull: false,
+         type: Sequelize.DATE
+       }
+      });
+    },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.dropTable('Skills');
+  }
+};
