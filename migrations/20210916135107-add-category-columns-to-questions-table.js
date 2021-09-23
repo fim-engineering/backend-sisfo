7cccb47675ce9e9d69d464600da9f669e5bc7b50
Luthfi Rahman <luthfirahman.id@gmail.com>
2021-09-24 00:02:16 +0700

    Edit migration and add seeder

--- migrations/20210916135107-add-category-columns-to-questions-table.js
+++ migrations/20210916135107-add-category-columns-to-questions-table.js
@@ -3,15 +3,12 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.addColumn('Questions', 'order', { type: Sequelize.INTEGER }),
       queryInterface.addColumn('Questions', 'category', { type: Sequelize.STRING })
-    ]).then(() => queryInterface.addIndex('Questions', ['order']))
-    
+    ])
   },
 
   down: async (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.removeColumn('Questions', 'order'),
       queryInterface.removeColumn('Questions', 'category')
     ])
   }
