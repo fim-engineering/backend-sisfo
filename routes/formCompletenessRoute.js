cf029f1a8b3fbb304e3d41f379898cf49eeecbea
Luthfi Rahman <luthfirahman.id@gmail.com>
2021-09-27 23:48:00 +0700

    Fixing form-completenes url

--- routes/formCompletenessRoute.js
+++ routes/formCompletenessRoute.js
@@ -4,7 +4,7 @@ const { body } = require('express-validator/check')
 const formCompletenessController = require('../controllers/formCompletenessController');
 const isAuth = require('../middleware/is-auth-middleware');
 
-router.get('/form-completeness', isAuth, formCompletenessController.getFormCompleteness);
-router.post('/form-completeness/submit', isAuth, formCompletenessController.submitFormCompleteness);
+router.get('', isAuth, formCompletenessController.getFormCompleteness);
+router.post('/submit', isAuth, formCompletenessController.submitFormCompleteness);
 
 module.exports = router;
